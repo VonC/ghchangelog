73ebb5df0512acc0d88fc6807f754593a92df2b2
VonC <vonc@laposte.net>
2022-09-25 12:11:23 +0200

    visit https://github.blog/changelog/

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"ghchangelog/version"
+	"log"
 	"os"
 	"strings"
+
+	"github.com/gocolly/colly/v2"
 )
 
 func main() {
@@ -20,4 +23,10 @@ func main() {
 		os.Exit(0)
 	}
 
+	c := colly.NewCollector(
+		colly.AllowedDomains("github.blog"),
+	)
+	if err := c.Visit("https://github.blog/changelog/"); err != nil {
+		log.Fatal(err)
+	}
 }
